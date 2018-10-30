f488ac4681835767b3727271050f20f26c5b2420
Antoni Dalle Nogare <antoni.dallenogare@gmail.com>
2018-10-30 15:18:58 +0100

    Updates

---
+++ precache-manifest.f485751a02029444103d4ff8a072acdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9e3bf1f61fd8760c0cc",
+    "url": "/React-video/static/js/runtime~main.e9e3bf1f.js"
+  },
+  {
+    "revision": "2a2fe44632374a367d9d",
+    "url": "/React-video/static/js/main.2a2fe446.chunk.js"
+  },
+  {
+    "revision": "a3e859d0c88d9e51b18c",
+    "url": "/React-video/static/js/1.a3e859d0.chunk.js"
+  },
+  {
+    "revision": "2a2fe44632374a367d9d",
+    "url": "/React-video/static/css/main.a06bf770.chunk.css"
+  },
+  {
+    "revision": "d24eb67852ed3500403b2bc9cc07fa89",
+    "url": "/React-video/index.html"
+  }
+];
