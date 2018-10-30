2f64ce426b1b3aa8a42f74cc323f37e96117f061
Antoni Dalle Nogare <antoni.dallenogare@gmail.com>
2018-10-30 15:00:52 +0100

    sprint 1

---
+++ src/Homepage.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import Showcard from './Showcard.jsx';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+// export default class Homepage extends Component {
+//
+//   render() {
+//     return (
+//       <div className="App">
+//       <h1>Homepage</h1>
+// <button type="button" onClick="BasicExample()" >Showcard</button>
+//
+//       </div>
+//
+//     );
+//   }
+//
+// }
+
+function BasicExample() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+          <button type="button"><Link to="/Showcard">Showcard</Link></button>
+
+          </li>
+
+        </ul>
+
+        <hr />
+
+        <Route exact path="/Showcard" component={Showcard} />
+
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+
+
+export default BasicExample;
---
+++ src/Showcard.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+import  {movie} from './movie.json';
+
+
+export default class Showcard extends Component {
+
+
+
+    render() {
+
+      return (
+          <ul>
+          {
+            movie.map( (movie) => {
+              return <li   key = { movie.Title }
+                >
+                <div className = 'card'>
+                <div className="image"><img src={movie.Poster}/>
+                </div>
+                <div className="content">
+                  <div className="title"><h5>{movie.Title}</h5></div>
+                  <div className="year"> <p>{movie.Year}</p></div>
+                  <div className="plot"> <p>{movie.Plot}</p></div>
+                </div>
+                </div>
+              </li>
+              ;
+            })
+          }
+          </ul>
+      );
+
+    }
+    
+  }
+
+
+
+
+
+
+//
+// export default Showcard;
